a837e0b3030a8cca6996bdb56a631851b7e5e307
Nikita Varankin <galfimber2014@yandex.ru>
2024-01-15 23:08:23 +0500

    Реализован калькулятор

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import popup from "./modules/popup";
 import slider from "./modules/slider";
 import timer from "./modules/timer";
 import scroll from "./modules/scroll";
+import calc from "./modules/calc";
 
 popup();
 slider();
 timer("20 january 2024");
 scroll();
+calc();
