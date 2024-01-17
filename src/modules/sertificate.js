2e96a3a2928dc3a01811b34b879ee27396446f45
Nikita Varankin <galfimber2014@yandex.ru>
2024-01-17 20:52:57 +0500

    Реализовано модальное окно сертификаты и документы

--- src/modules/sertificate.js
+++ src/modules/sertificate.js
@@ -4,28 +4,22 @@ const sertificate = () => {
   sertificates.forEach((sertificate) => {
     sertificate.addEventListener("click", (e) => {
       e.preventDefault();
-      //e.currentTarget.style.display = "inline-block";
-      //e.currentTarget.style.position = "fixed";
-      //e.currentTarget.style.top = "0";
-      //e.currentTarget.style.left = "0";
-      //e.currentTarget.style.right = "0";
-      //e.currentTarget.style.bottom = "0";
-      //e.currentTarget.style.zIndex = "999";
+
       const img = e.currentTarget.querySelector(".img-responsive");
       const overlay = e.currentTarget.querySelector(".document-overlay");
-      //overlay.style.opacity = "1";
-      //img.style.height = "90%";
-      //img.style.position = "absolute";
-      //overlay.style.zIndex = "998";
-      //img.style.zIndex = "999";
-      document.body.style.overflow = "hidden";
-      //   e.currentTarget.style.width = "100%";
-      //   e.currentTarget.style.height = "100%";
 
+      document.body.classList.toggle("body-block");
       e.currentTarget.classList.toggle("sertificate-document-active");
       img.classList.toggle("img-responsive-active");
       overlay.classList.toggle("document-overlay-active");
     });
+
+    sertificate.addEventListener("mouseover", (e) =>
+      overlayHover(e.target, true)
+    );
+    sertificate.addEventListener("mouseout", (e) =>
+      overlayHover(e.target, false)
+    );
   });
 };
 export default sertificate;
